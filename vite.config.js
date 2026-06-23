0c2230978567ee7c1c7546af944ad55250c44c26
Kate1jkee <voronina1996-1996k@mail.ru>
2026-06-23 17:22:42 +0300

    Build site for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/react-project/',
+  build: {
+    outDir: 'docs',
+  },
   plugins: [react()],
 })
