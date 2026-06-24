7e0ba3d8fa43c56fbdcdab36da5b3aeddc634842
Kate1jkee <voronina1996-1996k@mail.ru>
2026-06-24 15:56:49 +0300

    Изменение структуры файлов

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import Header from './components/header/component';
+import Footer from './components/footer/component';
+import { restaurants } from "../materials/mock";
+
+const App = ({ children }) => {
+   return (
+    <div>
+      <Header>Restaurants</Header>
+      {restaurants.map((restaurant) => (
+      <div>
+        <h2>{restaurant.name}</h2>
+        <div>
+         <h3>Меню</h3>
+         <ul>
+         {restaurant.menu.map((dish) => (
+          <li>{dish.name}</li>
+          ))}
+         </ul>
+       </div>
+       <div>
+        <h3>Отзывы</h3>
+         <ul>
+         {restaurant.reviews.map((review) => (
+          <li>{review.text}</li>
+          ))}
+         </ul>
+       </div>
+      </div>
+      ))}
+      <Footer>Contacts</Footer>
+    </div>
+   );
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -2,35 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import React from 'react'
-import { restaurants } from "../materials/mock";
-import Header from './components/header/component';
-import Footer from './components/footer/component';
+import App from './App'
 
 const root = createRoot (document.getElementById('root'));
 root.render(
-    <div>
-      <Header>Restaurants</Header>
-      {restaurants.map((restaurant) => (
-      <div>
-        <h2>{restaurant.name}</h2>
-        <div>
-         <h3>Меню</h3>
-         <ul>
-         {restaurant.menu.map((dish) => (
-          <li>{dish.name}</li>
-          ))}
-         </ul>
-       </div>
-       <div>
-        <h3>Отзывы</h3>
-         <ul>
-         {restaurant.reviews.map((review) => (
-          <li>{review.text}</li>
-          ))}
-         </ul>
-       </div>
-      </div>
-      ))}
-      <Footer>Contacts</Footer>
-    </div>
+      <StrictMode>
+    <App />
+  </StrictMode>
 );
