1c7cedc336f64c71190c554d08e2bfa207adfc34
Kate1jkee <voronina1996-1996k@mail.ru>
2026-07-01 14:25:27 +0300

    Добавление функции выбора ресторана с помощью useState

--- src/components/restaurants/component.jsx
+++ src/components/restaurants/component.jsx
@@ -1,11 +1,24 @@
 import Restaurant from '../restaurant/component';
+import { useState } from 'react';
 
 const Restaurants = ({restaurants}) => {
+  const [id, setId] = useState(restaurants[0].id);
+  const activeRestaurant = restaurants.find((restaurant) => restaurant.id === id);
+  
     return (
       <div>
         {restaurants.map((restaurant) => (
-          <Restaurant restaurant={restaurant}/>
+          <button onClick={() => {
+            if (restaurant.id === id) {
+              return;
+            }
+
+            setId(restaurant.id);
+          }}>
+            {restaurant.name}
+          </button>
          ))}
+         <Restaurant restaurant={activeRestaurant} />
       </div>
     );
 };
